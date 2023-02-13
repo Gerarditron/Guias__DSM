b795edf39d15fccc1ff1a3a6bc96ed0a9c9f53c3
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2023-02-12 22:42:11 -0600

    el primer commit

---
+++ Guia1/constantes.java
@@ -0,0 +1,9 @@
+public class Constantes{
+	public static void main (String[] args){
+		final float pi = 3.14159265f;
+		float radio = 3.5f;
+		double area;
+		
+		System.out.println("area circunferencia = " + pi*radio*radio);
+	}
+}
