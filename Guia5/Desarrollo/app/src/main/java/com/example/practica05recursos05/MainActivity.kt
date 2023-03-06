18747a5ed5f21904fb37255b2497fc0b9591f2a5
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2023-03-05 23:30:12 -0600

    Guia5 Completada

---
+++ Guia5/Desarrollo/app/src/main/java/com/example/practica05recursos05/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.practica05recursos05
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_muestra, menu)
+        return true
+    }
+
+}
