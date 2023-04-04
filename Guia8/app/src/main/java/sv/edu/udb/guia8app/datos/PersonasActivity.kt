e917ba6177bd0293ea0613ad6d8ba4946f427e9d
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2023-04-04 17:16:26 -0600

    Guia 8 avanzada

--- Guia8/app/src/main/java/sv/edu/udb/guia8app/datos/PersonasActivity.kt
+++ Guia8/app/src/main/java/sv/edu/udb/guia8app/datos/PersonasActivity.kt
@@ -8,11 +8,28 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
-import androidx.appcompact.app.AppCompatActivity
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
+import sv.edu.udb.guia8app.datos.Persona
 
 
 class PersonasActivity: AppCompatActivity() {
     //Ordenamiento para hacer la consultas a los datos
-    var consultaOrdenada:Query = refPersonas.orderByChild("nombre")
+    //var consultaOrdenada:Query = refPersona.orderByChild("nombre")
+    var persona: MutableList<Persona>?=null
+    var listaPersonas:ListView?=null
+
+    protected override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_personas)
+        inicializar()
+    }
+
+    private fun inicializar(){
+        //val fab_agregar:FloatingActionButton=findViewById<FloatingActionButton>(R.id.fab_agregar)
+        //listaPersonas = findViewById<ListView>(R.id.ListaPersonas)
+    }
+
+
 }
